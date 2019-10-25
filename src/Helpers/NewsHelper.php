ccbeb9ffc72351e762ba5aa573d585decb8ebf90
innoflash <innocentmazando@gmail.com>
2019-10-25 16:15:55 +0200

    added remote server access to non source projects

--- src/Helpers/NewsHelper.php
+++ src/Helpers/NewsHelper.php
@@ -6,15 +6,17 @@ namespace FaithGen\News\Helpers;
 
 use FaithGen\News\Models\News;
 use FaithGen\SDK\Helpers\Helper;
+use FaithGen\SDK\SDK;
 
 class NewsHelper extends Helper
 {
     public static $freeNewsCount = 2;
+
     public static function getAvatar(News $news)
     {
         return [
-            '_100' => asset('storage/news/100-100/' . $news->image->name),
-            'original' => asset('storage/news/original/' . $news->image->name),
+            '_100' => SDK::getAsset('storage/news/100-100/' . $news->image->name),
+            'original' => SDK::getAsset('storage/news/original/' . $news->image->name),
         ];
     }
 }
