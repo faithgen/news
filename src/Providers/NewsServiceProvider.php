e35c84681f9d2197bed1cc82a46ef6a90e60c327
innoflash <innocentmazando@gmail.com>
2019-10-26 07:05:07 +0200

    running config via trait

--- src/Providers/NewsServiceProvider.php
+++ src/Providers/NewsServiceProvider.php
@@ -4,11 +4,14 @@ namespace FaithGen\News\Providers;
 
 use FaithGen\News\Models\News;
 use FaithGen\News\Observers\Ministry\NewsObserver;
+use FaithGen\SDK\Traits\ConfigTrait;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class NewsServiceProvider extends ServiceProvider
 {
+    use ConfigTrait;
+
     /**
      * Bootstrap services.
      *
@@ -17,54 +20,42 @@ class NewsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config/faithgen-news.php', 'faithgen-news');
-        $this->registerRoutes();
+        $this->registerRoutes(__DIR__ . '/../routes/news.php', __DIR__ . '/../routes/source.php');
 
-        if ($this->app->runningInConsole()) {
-            if (config('faithgen-sdk.source')) {
-                $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-
-                $this->publishes([
-                    __DIR__ . '/../storage/news/' => storage_path('app/public/news')
-                ], 'faithgen-news-storage');
-
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/' => database_path('migrations')
-                ], 'faithgen-news-migrations');
-            }
+        $this->setUpFiles(function () {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
             $this->publishes([
-                __DIR__ . '/../config/faithgen-news.php' => config_path('faithgen-news.php')
-            ], 'faithgen-news-config');
-        }
+                __DIR__ . '/../storage/news/' => storage_path('app/public/news')
+            ], 'faithgen-news-storage');
+
+            $this->publishes([
+                __DIR__ . '/../database/migrations/' => database_path('migrations')
+            ], 'faithgen-news-migrations');
+        });
+
+        $this->publishes([
+            __DIR__ . '/../config/faithgen-news.php' => config_path('faithgen-news.php')
+        ], 'faithgen-news-config');
 
         News::observe(NewsObserver::class);
     }
 
-    private function registerRoutes()
+    public function register()
     {
-        Route::group($this->routeConfiguration(), function () {
-            $this->loadRoutesFrom(__DIR__ . '/../routes/news.php');
-            if (config('faithgen-sdk.source'))
-                $this->loadRoutesFrom(__DIR__ . '/../routes/source.php');
-        });
+
     }
 
-    private function routeConfiguration()
+    /**
+     * The config you want to be applied onto your routes
+     * @return array the rules eg, middleware, prefix, namespace
+     */
+    function routeConfiguration(): array
     {
         return [
             'prefix' => config('faithgen-news.prefix'),
             'namespace' => "FaithGen\News\Http\Controllers",
             'middleware' => config('faithgen-news.middlewares'),
         ];
-    }
-
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
