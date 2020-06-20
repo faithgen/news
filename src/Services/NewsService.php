02b0ef8473697859d3070ed388641142d026d87d
innoflash <innocentmazando@gmail.com>
2020-06-20 04:58:06 +0200

    updated service

--- src/Services/NewsService.php
+++ src/Services/NewsService.php
@@ -3,28 +3,30 @@
 namespace FaithGen\News\Services;
 
 use FaithGen\News\Models\News;
-use FaithGen\SDK\Traits\FileTraits;
 use InnoFlash\LaraStart\Services\CRUDServices;
 
 class NewsService extends CRUDServices
 {
-    use FileTraits;
     /**
      * @var News
      */
-    private $news;
+    protected News $news;
 
-    public function __construct(News $news)
+    public function __construct()
     {
-        if (request()->has('news_id')) {
-            $this->news = News::findOrFail(request()->news_id);
-        } else {
-            $this->news = $news;
+        $this->news = app(News::class);
+
+        $newsId = request()->route('news') ?? request('news_id');
+
+        if ($newsId) {
+            $this->news = $this->news->resolveRouteBinding($newsId);
         }
     }
 
     /**
-     * @return News
+     * Retrieves an instance of news.
+     *
+     * @return \FaithGen\News\Models\News
      */
     public function getNews(): News
     {
@@ -32,21 +34,15 @@ class NewsService extends CRUDServices
     }
 
     /**
-     * This sets the attributes to be removed from the given set for updating or creating.
-     * @return mixed
+     * Makes a list of fields that you do not want to be sent
+     * to the create or update methods.
+     * Its mainly the fields that you do not have in the messages table.
+     *
+     * @return array
      */
     public function getUnsetFields(): array
     {
-        return ['news_id', 'image'];
-    }
-
-    /**
-     * This get the model value or class of the model in the service.
-     * @return mixed
-     */
-    public function getModel()
-    {
-        return $this->getNews();
+        return ['news_id'];
     }
 
     public function getParentRelationship()
